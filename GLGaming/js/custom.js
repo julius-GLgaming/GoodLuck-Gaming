49a81df1111e35ee04fc8d3997ec0769aca0aa90
Julius Seok <julius@gl-gaming.com>
2017-01-26 17:19:06 -0800

    Initial source code pushed to Github repo GoodLuck-Gaming

---
+++ GLGaming/js/custom.js
@@ -0,0 +1,161 @@
+//dropdown
+
+function openGamingDropdown() {
+    document.getElementById("gamingSessions").classList.toggle("show");
+    var dropdowns = document.getElementsByClassName("dota-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+}
+
+function sendMail() {
+
+    var link = "mailto:contact@gl-gaming.com"
+             + "&subject=" + escape("Message from "+document.getElementById('senderAddress'))
+             + "&body=" + escape(document.getElementById('messageText').value)
+    ;
+    
+
+    window.open(link);
+   
+}
+
+function openDotaDropdown() {
+    //alert(document.getElementById("dotaCelebs").classList);
+    document.getElementById("dotaCelebs").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (event.target.matches('gamingDropdown')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+  if (event.target.matches('dotaList')) {
+    var dropdowns = document.getElementsByClassName("dota-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// ISOTOPE FILTER
+jQuery(document).ready(function($){
+
+  if ( $('.iso-box-wrapper').length > 0 ) { 
+
+      var $container  = $('.iso-box-wrapper'), 
+        $imgs     = $('.iso-box img');
+
+      $container.imagesLoaded(function () {
+
+        $container.isotope({
+        layoutMode: 'fitRows',
+        itemSelector: '.iso-box'
+        });
+
+        $imgs.load(function(){
+          $container.isotope('reLayout');
+        })
+
+      });
+
+      //filter items on button click
+
+      $('.filter-wrapper li a').click(function(){
+
+          var $this = $(this), filterValue = $this.attr('data-filter');
+
+      $container.isotope({ 
+        filter: filterValue,
+        animationOptions: { 
+            duration: 750, 
+            easing: 'linear', 
+            queue: false, 
+        }                
+      });             
+
+      // don't proceed if already selected 
+
+      if ( $this.hasClass('selected') ) { 
+        return false; 
+      }
+
+      var filter_wrapper = $this.closest('.filter-wrapper');
+      filter_wrapper.find('.selected').removeClass('selected');
+      $this.addClass('selected');
+
+        return false;
+      }); 
+
+  }
+
+});
+
+
+// PRELOADER JS
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+
+// jQuery to collapse the navbar on scroll //
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+$(function(){
+
+  // ------- WOW ANIMATED ------ //
+  wow = new WOW(
+  {
+    mobile: false
+  });
+  wow.init();
+
+
+  // HIDE MOBILE MENU AFTER CLIKING ON A LINK
+  $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  // NIVO LIGHTBOX
+  $('.iso-box-section a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+
+  // HOME BACKGROUND SLIDESHOW
+  $(function(){
+    jQuery(document).ready(function() {
+    $('#home').backstretch([
+       "images/home-bg-slideshow1.jpg", 
+       "images/home-bg-slideshow2.jpg",
+        ],  {duration: 2000, fade: 750});
+    });
+  })
+
+});
+
